6623e6c3e0a78b0b32f1352b5193a564883e390d
kaush <1042037+kaushikgopal@users.noreply.github.com>
2024-08-20 07:30:22 -0700

    package rename - closes #1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ java {
 
 application {
   // Define the main class for the application.
-  mainClass = "org.example.AppKt"
+  mainClass = "kau.sh.oss.testing.AppKt"
 }
 
 tasks.named<Test>("test") {
--- app/src/test/kotlin/kau/sh/oss/testing/CoroutineTestRule.kt
+++ app/src/test/kotlin/kau/sh/oss/testing/CoroutineTestRule.kt
@@ -1,4 +1,4 @@
-package org.example
+package kau.sh.oss.testing
 
 
 import kotlinx.coroutines.Dispatchers
